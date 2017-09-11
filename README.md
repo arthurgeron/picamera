dff70d0c74bf571afe61dd657eedaf33fabf55ce
Arthur Geron <arthurgeron@hotmail.com>
2017-09-11 01:32:26 -0300

    Initial release

---
+++ README.md
@@ -0,0 +1,13 @@
+#PiCamera
+=====================
+
+Project developed to turn any device into a security camera. This is a University project.
+
+##Usage
+Run <main.py> and open http://localhost:5000 on your web browser
+##Requirements
+An USB or integrated Camera
+Python3
+[Pip](https://pip.pypa.io/en/stable/installing/)
+[Flask](http://flask.pocoo.org/docs/0.12/installation/)
+[OpenCV_Python](https://pypi.python.org/pypi/opencv-python)
---
+++ camera_opencv.py
@@ -0,0 +1,19 @@
+import cv2
+from base_camera import BaseCamera
+
+
+class Camera(BaseCamera):
+    video_source = 0
+    
+    @staticmethod
+    def frames():
+        camera = cv2.VideoCapture(Camera.video_source)
+        if not camera.isOpened():
+            raise RuntimeError('Could not start camera.')
+
+        while True:
+            # read current frame
+            _, img = camera.read()
+
+            # encode as a jpeg image and return it
+            yield cv2.imencode('.jpg', img)[1].tobytes()
